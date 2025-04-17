eb9e5259b0d17b87bc353993a8c876be83bedfba
Maxim <toper.one111@gmail.com>
2025-04-17 12:12:41 +0300

    feat: implement our services

--- src/js/components/serviceItem.js
+++ src/js/components/serviceItem.js
@@ -1,12 +1,20 @@
 import $ from "jquery";
 
 $(document).ready(function () {
-  $(".service-item").each(function (index) {
-    $(this).on("mouseenter", function () {
-      $(".media-img").removeClass("active").fadeOut(600);
-      $(`.media-img[data-index="${index + 1}"]`)
+  const updateMedia = (index) => {
+    const targets = [".media-img", ".media-content"];
+
+    targets.forEach((selector) => {
+      $(selector).removeClass("active").fadeOut(600);
+      $(`${selector}[data-index="${index + 1}"]`)
         .addClass("active")
         .fadeIn(600);
+    });
+  };
+
+  $(".service-item").each(function (index) {
+    $(this).on("mouseenter", () => {
+      updateMedia(index);
     });
   });
 });
