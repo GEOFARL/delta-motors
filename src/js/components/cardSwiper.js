26b1ae3bfea803be2f5aadb8786054ed685c9d05
Maxim <toper.one111@gmail.com>
2025-04-24 09:06:31 +0300

    feat: add cars for u section

--- src/js/components/cardSwiper.js
+++ src/js/components/cardSwiper.js
@@ -16,6 +16,7 @@ $(document).ready(function () {
 
   initSwiper(".available-cars-swiper");
   initSwiper(".auction-cars-swiper");
+  initSwiper(".cars-for-u-swiper", 20);
   initSwiper(".main-image-swiper", 40, 1);
   initSwiper(".testimonials-swiper", 28);
 });
