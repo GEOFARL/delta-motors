8fd3686579996080317f21786f8f5354c47d0938
GEOFARL <GEOFARL@users.noreply.github.com>
2025-04-27 12:59:02 +0000

    deploy: 210b343ee33805bf10371c5029c37f3945dee431

--- js/index.js
+++ js/index.js
@@ -20665,12 +20665,32 @@
 	  });
 	};
 
+	const initCookieConsent = () => {
+	  const delay = 3000;
+
+	  if (
+	    !localStorage.getItem("cookiesAccepted") ||
+	    localStorage.getItem("cookiesAccepted") !== "true"
+	  ) {
+	    setTimeout(() => {
+	      $(".cookie-consent, .cookie-overlay").addClass("show");
+	    }, delay);
+	  }
+
+	  $(".cookie-consent__accept, .cookie-consent__close").on("click", function () {
+	    const isAccepted = $(this).hasClass("cookie-consent__accept");
+	    localStorage.setItem("cookiesAccepted", isAccepted ? "true" : "false");
+	    $(".cookie-consent, .cookie-overlay").removeClass("show");
+	  });
+	};
+
 	$(function () {
 	  initContact();
 	  initSmartForm();
 	  initModal({
 	    openModalSelector: "#open-contact-modal",
 	  });
+	  initCookieConsent();
 
 	  initAccordion({
 	    itemSelector: ".faq-item",
