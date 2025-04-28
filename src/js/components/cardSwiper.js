a1db372867b7078f53de6223c6d1e77bad21806b
Maxim <toper.one111@gmail.com>
2025-04-28 10:36:31 +0300

    feat: add responsiveness for testimonials

--- src/js/components/cardSwiper.js
+++ src/js/components/cardSwiper.js
@@ -49,7 +49,11 @@ $(document).ready(function () {
   initSwiper(".cars-for-u-swiper", 20);
   initSwiper(".main-image-swiper", 40, 1);
   initSwiper(".testimonials-swiper", 28, 3, {
-    1024: {
+    350: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    768: {
       slidesPerView: 2,
       spaceBetween: 40,
     },
