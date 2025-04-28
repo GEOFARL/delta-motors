859f2dd66120951e4649a630cef5cff40e4e434f
Maxim <toper.one111@gmail.com>
2025-04-28 12:50:50 +0300

    feat: add responsiveness for hero about us

--- src/js/pages/about.js
+++ src/js/pages/about.js
@@ -1,6 +1,8 @@
 import $ from "jquery";
 import "../components/cardSwiper";
+import { initMobileMenu } from "../components/mobileMenu";
 
 $(function () {
+  initMobileMenu();
   console.log("About page initialized");
 });
