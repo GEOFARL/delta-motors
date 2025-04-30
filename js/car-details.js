8a523be6358aff0a3b53de265857112535a0ea0f
GEOFARL <GEOFARL@users.noreply.github.com>
2025-04-30 18:55:22 +0000

    deploy: b7838f58839e43b7f0f70ce91b41e5eedd89b797

--- js/car-details.js
+++ js/car-details.js
@@ -20550,7 +20550,20 @@
 	      spaceBetween: 20,
 	    },
 	  });
-	  initSwiper(".cars-for-u-swiper", 20);
+	  initSwiper(".cars-for-u-swiper", 20, 2, {
+	    350: {
+	      slidesPerView: 1,
+	      spaceBetween: 20,
+	    },
+	    1024: {
+	      slidesPerView: 2,
+	      spaceBetween: 20,
+	    },
+	    1680: {
+	      slidesPerView: 3,
+	      spaceBetween: 20,
+	    },
+	  });
 	  initSwiper(".main-image-swiper", 40, 1);
 	  initSwiper(".testimonials-swiper", 28, 3, {
 	    350: {
