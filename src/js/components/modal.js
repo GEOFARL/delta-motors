3914cf3ccd8be64a1fc0d7674c2ecd4832457478
Maxym <toper.one111@gmail.com>
2025-05-10 01:53:25 +0300

    feat: create success and error modals

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -2,7 +2,7 @@ import $ from "jquery";
 
 export const initModal = ({ openModalSelector }) => {
   $(openModalSelector).on("click", function () {
-    $(".modal-overlay").addClass("active");
+    $("#contact-modal").addClass("active");
   });
 
   $(".modal__close, .modal-overlay").on("click", function (e) {
@@ -11,5 +11,18 @@ export const initModal = ({ openModalSelector }) => {
 
   $(".modal").on("click", function (e) {
     e.stopPropagation();
+  });
+
+  const $form = $(".contact-us-modal-content .smart-form");
+  $form.on("submit", function (e) {
+    e.preventDefault();
+    const isSuccess = Math.random() < 0.7;
+    $(".modal-overlay").removeClass("active");
+    $form.find(".error").removeClass("error");
+
+    setTimeout(() => {
+      const modalId = isSuccess ? "#success-modal" : "#failure-modal";
+      $(modalId).addClass("active");
+    }, 300);
   });
 };
