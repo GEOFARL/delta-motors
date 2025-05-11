a84445fa12f88e52c19f63f2cf7c93baad952a55
GEOFARL <GEOFARL@users.noreply.github.com>
2025-05-11 08:45:50 +0000

    deploy: 9f17a8ef26d7e7f276344a44ab226b9dc64c981a

--- js/catalog.js
+++ js/catalog.js
@@ -10736,12 +10736,47 @@
 	  });
 	}
 
+	const initModal = ({ openModalSelectors }) => {
+	  openModalSelectors.forEach((openModalSelector) => {
+	    $(openModalSelector).on("click", function () {
+	      $("#contact-modal").addClass("active");
+	      $("#filters-modal").addClass("active");
+	      $(".mobile-menu").removeClass("open");
+	      $("body").removeClass("menu-open");
+	    });
+	  });
+
+	  $(".modal__close, .modal-overlay").on("click", function (e) {
+	    $(".modal-overlay").removeClass("active");
+	  });
+
+	  $(".modal").on("click", function (e) {
+	    e.stopPropagation();
+	  });
+
+	  const $form = $(".contact-us-modal-content .smart-form");
+	  $form.on("submit", function (e) {
+	    e.preventDefault();
+	    const isSuccess = Math.random() < 0.7;
+	    $(".modal-overlay").removeClass("active");
+	    $form.find(".error").removeClass("error");
+
+	    setTimeout(() => {
+	      const modalId = isSuccess ? "#success-modal" : "#failure-modal";
+	      $(modalId).addClass("active");
+	    }, 300);
+	  });
+	};
+
 	$(function () {
 	  console.log("Catalog page initialized");
 	  updateCatalogPage();
 	  initMobileMenu();
 	  initDropdown();
 	  initHeader();
+	  initModal({
+	    openModalSelectors: ["#open-filters"],
+	  });
 	  $(window).on("hashchange", updateCatalogPage);
 	});
 
--- js/index.js
+++ js/index.js
@@ -20799,6 +20799,7 @@
 	  openModalSelectors.forEach((openModalSelector) => {
 	    $(openModalSelector).on("click", function () {
 	      $("#contact-modal").addClass("active");
+	      $("#filters-modal").addClass("active");
 	      $(".mobile-menu").removeClass("open");
 	      $("body").removeClass("menu-open");
 	    });
