1e203980f12916e5b0065994dc3f5900024402f6
GEOFARL <GEOFARL@users.noreply.github.com>
2025-05-14 14:03:46 +0000

    deploy: d04386d75b79d5f745ebae1b1074b7ebaa16c844

--- js/car-details.js
+++ js/car-details.js
@@ -20644,5 +20644,24 @@
 	  console.log("Car details page initialized");
 	});
 
+	$(window).on("scroll", function () {
+	  const scrollTop = $(window).scrollTop();
+	  const $imageContainer = $(".car-details__images-container");
+	  const $container = $(".car-details__container");
+
+	  const containerTop = $container.offset().top;
+	  const containerHeight = $container.outerHeight();
+	  const imageHeight = $imageContainer.outerHeight();
+
+	  const maxOffset = containerHeight - imageHeight;
+	  const scrollOffset = scrollTop - containerTop + 100;
+
+	  let marginTop = 0;
+	  if (scrollOffset > 0) {
+	    marginTop = Math.min(scrollOffset, maxOffset);
+	  }
+	  $imageContainer.css("margin-top", `${marginTop}px`);
+	});
+
 })();
 //# sourceMappingURL=car-details.js.map
